aad049c8a74017f0d9f81c35e4a8ddba313c71f0
nerdnotgeeknot <rubaitreshad@gmail.com>
2021-05-19 21:20:33 +0600

    user login front end

--- Design/login.php
+++ Design/login.php
@@ -1,9 +1,12 @@
+<?php
+  include 'php/login.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>SPMS(IUB)</title>
+    <title>SPMS | G4</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="assets/vendors/iconfonts/mdi/css/materialdesignicons.css" />
     <link rel="stylesheet" href="assets/vendors/css/vendor.addons.css" />
@@ -23,7 +26,7 @@
       <div class="row">
         <div class="col-12 logo-section">
           <a href="index.html" class="logo">
-            <img src="assets/images/c14b8e0817be4c4fbf3deba49d2f95dc (1).png" alt="logo" />
+            <img src="assets/images/logo.png" alt="logo" />
           </a>
         </div>
       </div>
@@ -33,26 +36,15 @@
             <div class="grid-body">
               <div class="row">
                 <div class="col-lg-7 col-md-8 col-sm-9 col-12 mx-auto form-wrapper">
-                  <form action="#">
+                  <form method="POST" action="php/login.php">
                     <div class="form-group input-rounded">
-                      <input type="text" class="form-control" placeholder="Username" />
+                      <input type="email" class="form-control" placeholder="Email" name="email" required/>
                     </div>
                     <div class="form-group input-rounded">
-                      <input type="password" class="form-control" placeholder="Password" />
-                    </div>
-                    <div class="form-inline">
-                      <div class="checkbox">
-                        <label>
-                          <input type="checkbox" class="form-check-input" />Remember me <i class="input-frame"></i>
-                        </label>
-                      </div>
+                      <input type="password" class="form-control" placeholder="Password" name="password" required/>
                     </div>
                     <button type="submit" class="btn btn-primary btn-block"> Login </button>
                   </form>
-                  <div class="signup-link">
-                    <p>Don't have an account yet?</p>
-                    <a href="#">Sign Up</a>
-                  </div>
                 </div>
               </div>
             </div>
@@ -60,7 +52,7 @@
         </div>
       </div>
       <div class="auth_footer">
-        <p class="text-muted text-center">© Group 4 SUCKA OWLKIDS inc 2021</p>
+        <p class="text-muted text-center">© Group 4 | G4 | 2021</p>
       </div>
     </div>
     <!--page body ends -->
